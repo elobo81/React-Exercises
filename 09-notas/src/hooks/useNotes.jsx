b8e2b77c2c5bcd4a0e27520ce2e12274c041df9e
Enzo Lobo <enzolobo@gmail.com>
2023-05-20 20:53:56 -0300

    agrego uso de UseEffect para utilizar local storage y función eliminar

--- 09-notas/src/hooks/useNotes.jsx
+++ 09-notas/src/hooks/useNotes.jsx
@@ -1,23 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-const list = [
-    {title:"Hola", message:"Mundo", id:"123"},
-    {title:"Hola de nuevo", message:"Mundo Mundial", id:"124"}
-];
+// const list = [
+//     {title:"Hola", message:"Mundo", id:"123"},
+//     {title:"Hola de nuevo", message:"Mundo Mundial", id:"124"}
+// ];
 
 const LOCALSTORAGE_KEY = "MIAPP.list";
 
 function useNotes(){
-    const [notes, setNotes] = useState(list);
+    const [notes, setNotes] = useState([]);
 
+    useEffect(()=>{
+        let localStorageSavedNotes = localStorage.getItem(LOCALSTORAGE_KEY);
+        if(localStorageSavedNotes){
+            setNotes(
+                JSON.parse(localStorageSavedNotes)
+            )
+        }
+    }, []);
+
+    useEffect(()=>{
+        if(!notes || notes.length == 0) return;
+        localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(notes))
+    }, [notes]);
+    
     function add(title, message){
         const id = Date.now();
         const noteObject = { title, message, id };
         setNotes([noteObject, ...notes]);
     }
 
+    function remove(id){
+        let NewNotes = notes.filter( note => note.id !== id );
+        setNotes(NewNotes);
+    }
+
     return{
-        notes
+        notes,
+        add,
+        remove
     }
 }
 
--- 09-notas/src/notes/NotesList.jsx
+++ 09-notas/src/notes/NotesList.jsx
@@ -10,7 +10,7 @@ return (
     <div>
         <p style={{textAlign: "right"}}>Tienes {notes.length} notas guardadas</p>
         {
-            notes.map(note => <Note key={note.id} title={note.title} message={note.message} />)
+            notes.map(note => <Note key={note.id} id={note.id} title={note.title} message={note.message} />)
         }
     </div>
 )
